f84e684be10f8bbb4bf8021898a390e806505fd7
miltonAlan <miltonpaucar999@gmail.com>
2022-02-28 20:23:04 -0500

    MPaucar feat: Se ajustan campos tablas

--- createTables.sql
+++ createTables.sql
@@ -11,7 +11,7 @@ create table af_concepto
   ac_consecutivo double precision NOT NULL,
   ac_concepto character varying(255),
   ac_depreciable character varying(1),
-  ac_estado character(1),
+  ac_estado character varying(1),
   CONSTRAINT af_concepto_pkey PRIMARY KEY (ac_consecutivo),
   CONSTRAINT af_concepto_ac_concepto_key UNIQUE (ac_concepto)
 );
@@ -20,13 +20,13 @@ create table af_concepto
 create table af_usuario
 (
   au_consecutivo double precision NOT NULL,
-  au_nombre character(100),
-  au_apellido character(100),
-  au_fecha_nacimiento character(100),
-  au_clave character(100),
-  au_login character(100),
-  au_cedula character(50),
-  au_cargo character(100),
+  au_nombre character varying(100),
+  au_apellido character varying(100),
+  au_fecha_nacimiento character varying(100),
+  au_clave character varying(100),
+  au_login character varying(100),
+  au_cedula character varying(50),
+  au_cargo character varying(100),
   CONSTRAINT af_usuario_pkey PRIMARY KEY (au_consecutivo)
 );
 
@@ -34,15 +34,15 @@ create table af_usuario
 create table af_activo_fijo
 (
   af_consecutivo double precision NOT NULL,
-  af_estado character(1),
-  af_marca character(100),
-  af_modelo character(100),
-  ac_af_concepto character(100),
+  af_estado character varying(1),
+  af_marca character varying(100),
+  af_modelo character varying(100),
+  ac_af_concepto character varying(100),
   af_valor double precision,
   af_dep_acum double precision,
-  af_codigo_barras character(100),
+  af_codigo_barras character varying(100),
   af_periodo_dep double precision,
-  af_fecha_creacion character(100),
+  af_fecha_creacion character varying(100),
   au_af_consecutivo double precision,
   CONSTRAINT af_activo_fijo_pkey PRIMARY KEY (af_consecutivo),
   CONSTRAINT af_activo_fijo_ac_af_concepto_fkey FOREIGN KEY (ac_af_concepto)
@@ -58,12 +58,12 @@ create table af_activo_fijo
 create table af_historico
 (
   af_ah_consecutivo double precision NOT NULL,
-  ah_fecha character(100) NOT NULL,
-  ah_movimiento character(100) NOT NULL,
+  ah_fecha character varying(100) NOT NULL,
+  ah_movimiento character varying(100) NOT NULL,
   ah_valor double precision,
   ah_periodo double precision,
-  ah_responsable character(100),
-  CONSTRAINT af_historico_pkey PRIMARY KEY (af_ah_consecutivo, ah_fecha, ah_movimiento),
+  ah_responsable character varying(100),
+  CONSTRAINT af_historico_pkey PRIMARY KEY (af_ah_consecutivo),
   CONSTRAINT af_historico_af_ah_consecutivo_fkey FOREIGN KEY (af_ah_consecutivo)
       REFERENCES af_activo_fijo (af_consecutivo) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
@@ -75,6 +75,6 @@ create table af_variables
   av_consecutivo double precision NOT NULL,
   av_variable character varying(255),
   av_descripcion character varying(255),
-  av_valor character(255),
+  av_valor character varying(255),
   CONSTRAINT af_variables_pkey PRIMARY KEY (av_consecutivo)
 );

4609bed930598831a505d9c252fe7a4360076d70
miltonAlan <miltonpaucar999@gmail.com>
2022-03-19 14:20:53 -0500

    MPaucar feat: Ajuste campos tablas

--- inserData.sql
+++ inserData.sql
@@ -9,6 +9,8 @@ insert into af_concepto(ac_consecutivo, ac_concepto,   ac_depreciable, ac_estado
                  values(3,              'Terrenos',   'No',            'Vigente');
 insert into af_concepto(ac_consecutivo, ac_concepto,   ac_depreciable, ac_estado)
                  values(4,              'Tecnologia', 'Si',            'Vigente');
+insert into af_concepto(ac_consecutivo, ac_concepto,            ac_depreciable,   ac_estado)
+                 values(5,              'Varios', 'Si',            'Vigente');
 
 --ingreso usuarios
 delete from af_usuario;
@@ -44,3 +46,79 @@ values ('9', 'Xavier', 'Hernandez', '16/03/2022', 'fig656', 'XaviB6', '173225645
 insert into af_usuario (au_consecutivo, au_nombre, au_apellido, au_fecha_nacimiento, au_clave, au_login, au_cedula, au_cargo) 
 values ('10', 'Nicole', 'Chicaiza', '15/03/2022', 'caraM12', 'nicole0998', '1725138522', 'Administrador'); 
 
+--ingreso activos fijos
+delete from af_activo_fijo;
+
+insert into af_activo_fijo(af_consecutivo, af_estado,        af_marca,         af_modelo,         ac_af_concepto, 
+                           af_valor,       af_dep_acum,      af_codigo_barras, af_periodo_dep,    af_fecha_creacion, 
+                           au_af_consecutivo)
+                    values(1,              'Vigente',        'Mazda',          'Sport',             1, 
+                           10000,           0,                 '132165456',     0,               '01/12/2021',
+                           1);                           
+
+insert into af_activo_fijo(af_consecutivo, af_estado,        af_marca,         af_modelo,         ac_af_concepto, 
+                           af_valor,       af_dep_acum,      af_codigo_barras, af_periodo_dep,    af_fecha_creacion, 
+                           au_af_consecutivo)
+                    values(2,              'Vigente',        'Iphone',          'PRO MAX',             4, 
+                           1999,           0,                 '654654564',       0,               '28/03/2022',
+                           2);
+                           
+insert into af_activo_fijo(af_consecutivo, af_estado,        af_marca,         af_modelo,         ac_af_concepto, 
+                           af_valor,       af_dep_acum,      af_codigo_barras, af_periodo_dep,    af_fecha_creacion, 
+                           au_af_consecutivo)
+                    values(3,              'Vigente',        'Epson',          'Matricial a color',             4, 
+                           250,           0,                 '654567',        0,                '01/16/2022',
+                           10);        
+
+insert into af_activo_fijo(af_consecutivo, af_estado,        af_marca,         af_modelo,         ac_af_concepto, 
+                           af_valor,       af_dep_acum,      af_codigo_barras, af_periodo_dep,    af_fecha_creacion, 
+                           au_af_consecutivo)
+                    values(4,              'Vigente',        'Lenovo',          'Laptop IdealPad S340',   4, 
+                           900,           0,                '655644564',       0,                '09/22/2020',
+                           9);  
+
+insert into af_activo_fijo(af_consecutivo, af_estado,        af_marca,         af_modelo,         ac_af_concepto, 
+                           af_valor,       af_dep_acum,      af_codigo_barras, af_periodo_dep,    af_fecha_creacion, 
+                           au_af_consecutivo)
+                    values(5,              'Vigente',        'LG',          'Monitor led 32 PG',   4, 
+                           99,           0,                '897798987',       0,                '01/15/2019',
+                           6);                                                                                 
+
+insert into af_activo_fijo(af_consecutivo, af_estado,        af_marca,         af_modelo,         ac_af_concepto, 
+                           af_valor,       af_dep_acum,      af_codigo_barras, af_periodo_dep,    af_fecha_creacion, 
+                           au_af_consecutivo)
+                    values(6,              'Vigente',        'ARPEX',          'Archivo vertical de metal',   5, 
+                           12,           0,                '141234234234',       0,                '02/19/2010',
+                           6);  
+
+insert into af_activo_fijo(af_consecutivo, af_estado,        af_marca,         af_modelo,         ac_af_concepto, 
+                           af_valor,       af_dep_acum,      af_codigo_barras, af_periodo_dep,    af_fecha_creacion, 
+                           au_af_consecutivo)
+                    values(7,              'Vigente',        'ATLAS',          'Refrigeradora una puerta',   5, 
+                           238,           0,                '234222525',       0,                '02/12/2022',
+                           6);  
+insert into af_activo_fijo(af_consecutivo, af_estado,        af_marca,         af_modelo,         ac_af_concepto, 
+                           af_valor,       af_dep_acum,      af_codigo_barras, af_periodo_dep,    af_fecha_creacion, 
+                           au_af_consecutivo)
+                    values(8,              'Vigente',        'AXIS',          'Camara de seguridad 360',   4, 
+                           230,           0,                '654654',       0,                '01/08/2009',
+                           8);                             
+
+insert into af_activo_fijo(af_consecutivo, af_estado,        af_marca,         af_modelo,         ac_af_concepto, 
+                           af_valor,       af_dep_acum,      af_codigo_barras, af_periodo_dep,    af_fecha_creacion, 
+                           au_af_consecutivo)
+                    values(9,              'Vigente',        'ATLAS',          'Estanteria de metal',   5, 
+                           230,           0,                '1233313',       0,                '01/09/2001',
+                           2);  
+insert into af_activo_fijo(af_consecutivo, af_estado,        af_marca,         af_modelo,         ac_af_concepto, 
+                           af_valor,       af_dep_acum,      af_codigo_barras, af_periodo_dep,    af_fecha_creacion, 
+                           au_af_consecutivo)
+                    values(10,              'Vigente',        'CONDULAX',          'Cableado estructurado',   5, 
+                           2500,           0,                '9878964',       0,                '01/09/1999',
+                           3);      
+insert into af_activo_fijo(af_consecutivo, af_estado,        af_marca,         af_modelo,         ac_af_concepto, 
+                           af_valor,       af_dep_acum,      af_codigo_barras, af_periodo_dep,    af_fecha_creacion, 
+                           au_af_consecutivo)
+                    values(11,              'Vigente',        'N/A',          'Terreno 2000 ha',   3, 
+                           25000,           0,                '123413212',       0,                '01/09/1999',
+                           1);                                                                                   
